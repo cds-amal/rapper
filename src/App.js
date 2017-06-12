a68ec77bdc5102263e1386325a55e7e23d714d2d
Amal Sudama <cds.sudama@gmail.com>
2017-06-12 12:24:43 -0400

    bootstrap ui

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ import {
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    const BASE_URL = 'https://api.spotify.com/v1/search?q=';
+    const FETCH_URL = BASE_URL + this.state.query + '&type=artist&limit=1';
+
+    console.log('FETCH_URL', FETCH_URL);
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,9 +35,19 @@ class App extends Component {
             <FormControl
               type="text"
               placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
             />
-            <InputGroup.Addon>
+            <InputGroup.Addon
+              onClick={() => this.search()}
+            >
               <Glyphicon glyph="search"/>
+
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
