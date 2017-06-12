4449cc1f8cefe46b8ed766687632aa9ec48cc68c
Amal Sudama <cds.sudama@gmail.com>
2017-06-12 09:24:22 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        Music Man from App
+      </div>
+    );
+  }
+}
+
+export default App;
