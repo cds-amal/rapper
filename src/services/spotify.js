7b2f3b26f3e467abfe7031a9fd3495de31396291
Amal Sudama <cds.sudama@gmail.com>
2017-06-14 09:51:50 -0400

    log

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -9,6 +9,7 @@ const SEARCH_API = `${SPOTIFY_API}search?q=`;
 const secret = `${config.client_id}:${config.client_secret}`;
 const auth_url = 'https://accounts.spotify.com/api/token';
 const Authorization = `Basic ${base64.encode(secret)}`;
+console.log(secret)
 
 const cfg = {
   headers: {
