7104fe7a5a0f0b5d1bd8d2103fdf6a7e9f314e0f
Amal Sudama <cds.sudama@gmail.com>
2017-06-15 13:38:53 -0400

    remove trace statement

--- src/profile.js
+++ src/profile.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 class Profile extends Component {
 
   render() {
-    console.log('this.props', this.props);
-
     let artist = {
       name: '',
       followers: {total: ''},
